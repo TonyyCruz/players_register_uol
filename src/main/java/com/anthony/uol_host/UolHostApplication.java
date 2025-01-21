a680b73b83b95e46b34fd47121a216078e2996ab
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-01-21 18:01:14 -0300

    INITIAL COMMIT

---
+++ src/main/java/com/anthony/uol_host/UolHostApplication.java
@@ -0,0 +1,13 @@
+package com.anthony.uol_host;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UolHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UolHostApplication.class, args);
+	}
+
+}
