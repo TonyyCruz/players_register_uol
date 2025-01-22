2e8b38e7660fa999b11f38d89dbb82c5a4ec1407
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-01-22 12:17:59 -0300

    remove unutilized code

--- src/main/java/com/anthony/uol_host/UolHostApplication.java
+++ src/main/java/com/anthony/uol_host/UolHostApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UolHostApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UolHostApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(UolHostApplication.class, args);}
 
 }
