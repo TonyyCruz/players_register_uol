b6907a1934f9f889588b2dc9e98dc69ce2185606
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-01-23 19:31:55 -0300

    refactor: change jogador to player

--- src/main/java/com/anthony/uol_host/model/Jogador.java
+++
@@ -1,9 +0,0 @@
-package com.anthony.uol_host.model;
-
-import com.anthony.uol_host.enums.GrupoCodinome;
-
-public record Jogador(String nome, String email, String telefone, String codinome, GrupoCodinome grupo) {
-    public Jogador(String nome, String email, String codinome, GrupoCodinome grupo) {
-        this(nome, email, "", codinome, grupo);
-    }
-}
