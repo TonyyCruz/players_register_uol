e79e39e06db8f7e906fdd15b3c137ae2ff5cbbc0
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-01-29 17:56:05 -0300

    feat: create codename service

--- src/main/java/com/anthony/uol_host/service/CodenameService.java
+++ src/main/java/com/anthony/uol_host/service/CodenameService.java
@@ -1,10 +1,10 @@
 package com.anthony.uol_host.service;
 
 import com.anthony.uol_host.enums.GroupCodename;
+import com.anthony.uol_host.repository.ICodenameRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -13,18 +13,23 @@ import java.util.Random;
 public class CodenameService {
     private final CodenameRepositoryFactory codenameRepositoryFactory;
 
-    public String createCodename(GroupCodename groupCodename, List<String> usedCodenames) {
-        List<String> availableCodenames = listAvailableCodenames(usedCodenames);
+    public String createCodename(GroupCodename groupCodename, List<String> usedCodenames) throws Exception {
+        List<String> availableCodenames = listAvailableCodenames(groupCodename, usedCodenames);
         if (availableCodenames.isEmpty()) {
             throw new RuntimeException("No available codenames");
         }
         return getRandomItemInList(availableCodenames);
     }
 
-    private List<String> listAvailableCodenames(List<String> usedCodenames) {
-        List<String> availableCodenames = codenameRepositoryFactory.create(GroupCodename.AVENGERS).getCodenames();
+    private List<String> listAvailableCodenames(GroupCodename groupCodename, List<String> usedCodenames) throws Exception {
+        List<String> availableCodenames = getCodenames(groupCodename);
         availableCodenames.removeAll(usedCodenames);
         return availableCodenames;
+    }
+
+    private List<String> getCodenames(GroupCodename groupCodename) throws Exception {
+        ICodenameRepository codenameRepository = codenameRepositoryFactory.create(groupCodename);
+        return codenameRepository.getCodenames();
     }
 
     private String getRandomItemInList(List<String> list) {
