1b98187126d3884f17cc478d4db79b6e95ef4ddc
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-01-31 18:01:53 -0300

    feat: add player service

--- src/main/java/com/anthony/uol_host/model/Player.java
+++ src/main/java/com/anthony/uol_host/model/Player.java
@@ -3,7 +3,7 @@ package com.anthony.uol_host.model;
 import com.anthony.uol_host.enums.GroupCodename;
 
 public record Player(String name, String email, String phone, String codename, GroupCodename group) {
-    public Player(String name, String email, String codename, GroupCodename group) {
-        this(name, email, "", codename, group);
-    }
+//    public Player(String name, String email, String codename, GroupCodename group) {
+//        this(name, email, "", codename, group);
+//    }
 }
