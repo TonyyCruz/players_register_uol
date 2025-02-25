c2ceca4bb042162db8fe7715d60a0a9118de4ebd
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-02-25 18:30:21 -0300

    feat: add validation to players registration

--- src/main/java/com/anthony/uol_host/controller/PlayerRegistrationController.java
+++ src/main/java/com/anthony/uol_host/controller/PlayerRegistrationController.java
@@ -3,8 +3,10 @@ package com.anthony.uol_host.controller;
 import com.anthony.uol_host.enums.GroupCodename;
 import com.anthony.uol_host.model.Player;
 import com.anthony.uol_host.service.PlayerService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,18 +23,21 @@ public class PlayerRegistrationController {
 
     @GetMapping
     public String pagePlayerRegistration(Model model) {
-        model.addAttribute("player", new Player(null, null, null, null, null));
-        model.addAttribute("groupsCodenames", GroupCodename.values());
-        return "player_registration";
+        return getViewAndModel(model, new Player(null, null, null, null, null));
     }
 
     @PostMapping
-    public String registerPlayer(@ModelAttribute Player player) {
-        try {
-            playerService.create(player);
-            return "redirect:/player-registration";
-        } catch (Exception e) {
-            return "redirect:/player-registration";
+    public String registerPlayer(@ModelAttribute @Valid Player player, BindingResult result, Model model) throws Exception {
+        if (result.hasErrors()) {
+            return getViewAndModel(model, player);
         }
+        playerService.create(player);
+        return "redirect:/player-registration";
+    }
+
+    private static String getViewAndModel(Model model, Player player) {
+        model.addAttribute("player", player);
+        model.addAttribute("groupsCodenames", GroupCodename.values());
+        return "player_registration";
     }
 }
--- src/main/java/com/anthony/uol_host/model/Player.java
+++ src/main/java/com/anthony/uol_host/model/Player.java
@@ -1,5 +1,15 @@
 package com.anthony.uol_host.model;
 
 import com.anthony.uol_host.enums.GroupCodename;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
-public record Player(String name, String email, String phone, String codename, GroupCodename groupCodename) {}
+@Validated
+public record Player(
+        @NotBlank(message = "Name is required.") String name,
+        @NotBlank(message = "Email is required.") @Email(message = "Format invalid.") String email,
+        String phone,
+        String codename,
+        @NotNull(message = "Please select a group.") GroupCodename groupCodename) {}
